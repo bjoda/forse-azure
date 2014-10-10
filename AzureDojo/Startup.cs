5cf8ac82f6ff0f93e240571059e0eb337bb073c5
bjoda <bjorn@donc.se>
2014-10-10 13:33:23 +0200

    First commit

---
+++ AzureDojo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AzureDojo.Startup))]
+namespace AzureDojo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
